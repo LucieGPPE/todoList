f674e7079b980d16e8a83e409371f1c9506139bd
Maël Robert <contact@mrbt.fr>
2024-05-25 15:22:35 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    uLoadScreen()
+    setTimeout(dLoadScreen, 2000)
+    setTimeout(showApp, 2000)
+
+    document.querySelector('.add').addEventListener('click', () => {
+        document.querySelector('.addTaskForm').style.zIndex = "1";
+        anime({
+            targets: '.addTaskForm',
+            opacity: [0, 1],
+            translateY: [250, 0],
+            duration: 2000
+        });
+    });
+});
+
+const uLoadScreen = () => {
+    anime({
+        targets: '.loader',
+        opacity: [0, 1],
+        translateY: [250, 0],
+        duration: 2000
+    });
+}
+
+const dLoadScreen = () => {
+    anime({
+        targets: '.loader',
+        opacity: [1, 0],
+        translateY: [0, -250],
+        duration: 5000,
+        easing: 'spring(1, 80, 10, 0)'
+    })
+}
+
+const showApp = () => {
+    anime({
+        targets: '.tasks',
+        opacity: [0, 1],
+        translateX: [250, 0],
+        duration: 4000,
+        easing: 'spring(1, 80, 10, 0)'
+    })
+}
